450d1ffe761768bd3a8a054b2a84816d78dcdcca
Jhonny <gonzodzen@mail.ru>
2022-11-28 17:58:30 +0300

    Return Early Pattern for Functions from freecodecamp

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-function isLess(a, b) {
+// Setup
+function abTest(a, b) {
     // Only change code below this line
-    return a < b
-     
+  
+  
+  
     // Only change code above this line
+  
+    return a<0||b<0?undefined:Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
   }
   
-  isLess(10, 15);
+  abTest(2,2);
