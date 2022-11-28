9d387234c0fc822a68e6532cf250b070de04ede8
Jhonny <gonzodzen@mail.ru>
2022-11-28 19:18:30 +0300

    Delete Properties from a JavaScript Object from freecodecamp

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+// Setup
 const myDog = {
     "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
-    "friends": ["freeCodeCamp Campers"]
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
   };
-  myDog.bark = "bow-wow"
+  delete myDog.tails
   
+  // Only change code below this line
   
