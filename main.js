8bf54227fa7984a083555fef2b9f364c632470f6
Jhonny <gonzodzen@mail.ru>
2022-11-28 18:23:01 +0300

    Build JavaScript Objects from freecodecamp

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-let count = 0;
-function cc(card) {
-  switch(card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case 'J':
-    case 'Q':
-    case 'K':
-    case 'A':
-      count--;
-  }
-  if(count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
-}
+const myDog = {
+    // Only change code below this line
+  name: 'Dog',
+  legs: 4,
+  tails: 1,
+  friends: [1,2,3,4]
+  
+    // Only change code above this line
+  };
