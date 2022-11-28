8760c138da4279dcf619a561af13ae3f07e0efd4
Jhonny <gonzodzen@mail.ru>
2022-11-28 19:46:30 +0300

    Accessing Nested Arrays from freecodecamp

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-const myMusic = [
-    {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CS",
-        "8T",
-        "LP" ],
-      "gold": true
-    },
-    {
-      "artist": "ABBA",
-      "title": "Vasia",
-      "release_year": 1989,
-      "formats": [
-        "CS",
-        "8T",
-        "LP",
-      "CD",
-    ]
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
     }
-  ];
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
