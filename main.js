42c890162d701ae87d74bc468c4c858bd0ca8bc3
Jhonny <gonzodzen@mail.ru>
2022-11-28 21:16:53 +0300

    Profile Lookup from freecodecamp

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-function sum(arr, n) {
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
     // Only change code below this line
-  if(n<=0){
-    return n
-  }
-  else{
-    return sum(arr, n-1) + arr[n-1]
-  }
+  
     // Only change code above this line
   }
-  console.log(sum([2, 3, 4, 5], 3))
+  
+  lookUpProfile("Akira", "likes");
