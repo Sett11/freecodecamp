00b845ab80918a28b2f7c215746858af2d556637
Jhonny <gonzodzen@mail.ru>
2022-11-28 15:43:19 +0300

    Local Scope and Functions from freecodecamp

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('inside myLocalScope', myVar);
+function myLocalScope() {
+    // Only change code below this line
+  let myVar;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
