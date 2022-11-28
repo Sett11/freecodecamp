89c73f111cfcdae4733ebe898c607fd7bc8c9de8
Jhonny <gonzodzen@mail.ru>
2022-11-28 15:49:46 +0300

    Assignment with a Returned Value from freecodecamp

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 // Setup
-let sum = 0;
-
-function addThree() {
-  sum = sum + 3;
-}
-function addFive(){
-  sum+=5
-}
-// Only change code below this line
 
 
-// Only change code above this line
-
-addThree();
-addFive();
+function processArg(num) {
+    return (num + 3) / 5;
+  }
+  
+  // Only change code below this line
+  const processed = processArg(7)
