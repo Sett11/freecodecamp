8ec03a8918ef2a919b5a735def90a1d711fe7727
Jhonny <gonzodzen@mail.ru>
2022-11-28 21:23:22 +0300

    Generate Random Whole Numbers within a Range from freecodecamp

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-function randomFraction() {
-
+function randomRange(myMin, myMax) {
     // Only change code below this line
-  
-    return Math.random()
-  
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
     // Only change code above this line
   }
