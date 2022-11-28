f23bc3c3e9de5350709edc8264ddc3081061469a
Jhonny <gonzodzen@mail.ru>
2022-11-28 20:21:18 +0300

    Iterate with JavaScript While Loops from freecodecamp

--- main.js
+++ main.js
@@ -1,49 +1,10 @@
 // Setup
-const recordCollection = {
-    "2548": {
-      "album": "Slippery When Wet",
-      "artist": "Bon Jovi",
-      "tracks": [
-        "Let It Rock",
-        "You Give Love a Bad Name"
-      ]
-    },
-    "2468": {
-      "album": "1999",
-      "artist": "Prince",
-      "tracks": [
-        "1999",
-        "Little Red Corvette"
-      ]
-    },
-    "1245": {
-      "artist": "Robert Palmer",
-      "tracks": [ ]
-    },
-    "5439": {
-      "album": "ABBA Gold"
-    }
-};
-// Keep a copy of the collection for tests
-var collectionCopy = JSON.parse(JSON.stringify(recordCollection));
-
-// Only change code below this line
-function updateRecords(id, prop, value) {
-
-  if (value === '') {
-    delete recordCollection[id][prop]; // If the value is empty remove the prop
-  } else if (prop !== "tracks") {
-    recordCollection[id][prop] = value;
-  } else {
-    if (!recordCollection[id].hasOwnProperty('tracks')) {
-      recordCollection[id].tracks = [];
-      recordCollection[id].tracks.push(value);
-    } else {
-      recordCollection[id].tracks.push(value);
-    }
-  }
-  return recordCollection;
+const myArray = [];
+let i = 5
+while(i>=0){
+  myArray.push(i)
+  i--
 }
 
-// Alter values below to test your code
-updateRecords(2468, "tracks", "Free");
+// Only change code below this line
+console.log(myArray)
