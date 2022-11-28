a04a3c441c0505a8a9ceb5a8138cf98b78f8017a
Jhonny <gonzodzen@mail.ru>
2022-11-28 19:26:59 +0300

    Testing Objects for Properties from freecodecamp

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-// Setup
-function phoneticLookup(val) {
-    let result = "";
-  
+function checkObj(obj, checkProp) {
     // Only change code below this line
-    switch(val) {
-      case "alpha":
-        result = "Adams";
-        break;
-      case "bravo":
-        result = "Boston";
-        break;
-      case "charlie":
-        result = "Chicago";
-        break;
-      case "delta":
-        result = "Denver";
-        break;
-      case "echo":
-        result = "Easy";
-        break;
-      case "foxtrot":
-        result = "Frank";
-    }
-  
+    return obj.hasOwnProperty(checkProp)?obj[checkProp]:"Not Found";
     // Only change code above this line
-    return result;
   }
-  
-  phoneticLookup("charlie");
+  console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift"))
