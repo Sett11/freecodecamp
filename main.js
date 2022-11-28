1307566b1fd17b4a72bb7cb5a90687c5b6e003f4
Jhonny <gonzodzen@mail.ru>
2022-11-28 18:12:40 +0300

    Counting Cards from freecodecamp

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-// Setup
-function abTest(a, b) {
-    // Only change code below this line
-  
-  
-  
-    // Only change code above this line
-  
-    return a<0||b<0?undefined:Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+let count = 0;
+function cc(card) {
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
   }
-  
-  abTest(2,2);
+  if(count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
