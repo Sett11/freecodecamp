101e1f11118de00daf8c7cdf5537653211f27adf
Jhonny <gonzodzen@mail.ru>
2022-11-28 17:19:16 +0300

    Golf Code from freecodecamp

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-function testSize(num) {
+function golfScore(par, strokes) {
     // Only change code below this line
-  if(num<5){
-    return 'Tiny'
-  }
-  else if(num<10&&num>=5){
-    return 'Small'
-  }
-  else if(num<15&&num>=10){
-    return 'Medium'
-  }
-  else if(num<20&&num>=15){
-    return 'Large'
-  }
-  else if(num>=20){
-    return 'Huge'
-  }
   
-    return "Change Me";
+    if (strokes === 1) return "Hole-in-one!";
+  
+    else if ((strokes - par) <= -2) return 'Eagle';
+  
+    else if ((strokes - par) === -1) return 'Birdie';
+  
+    else if (strokes === par) return 'Par';
+  
+    else if ((strokes - par) === 1) return 'Bogey';
+  
+    else if ((strokes - par) === 2) return 'Double Bogey';
+  
+    else return 'Go Home!';
     // Only change code above this line
   }
   
-  testSize(7);
+  golfScore(5, 4);
