2e3b04c29a0e127ed81f9da6be5e0fc82574dec4
Jhonny <gonzodzen@mail.ru>
2022-11-28 21:19:46 +0300

    Generate Random Whole Numbers with JavaScript from freecodecamp

--- main.js
+++ main.js
@@ -1,52 +1,8 @@
-// Setup
-const contacts = [
-    {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-  ];
+function randomFraction() {
+
+    // Only change code below this line
   
-  function lookUpProfile(firstName, prop){
-  // Only change code below this line
-    var contact;
-    
-    for (var i=0; i<contacts.length; i++) {
-      if (contacts[i].firstName === firstName) {
-        contact = contacts[i];
-        break;
-      }
-    }
-    
-    if (contact) {
-      if (contact.hasOwnProperty(prop)) {
-        return contact[prop];
-      } else {
-        return "No such property";
-      }
-    } else {
-      return "No such contact";
-    }
-  // Only change code above this line
+    return Math.random()
+  
+    // Only change code above this line
   }
-  
-  lookUpProfile("Akira", "likes");
