790707bf9208d051027f248baa1a8d2b4fae1148
Jhonny <gonzodzen@mail.ru>
2022-11-28 14:27:49 +0300

    Find the Length of a String from freecodecamp

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-const someAdjective = "fack";
-let myStr = "Learning to code is ";
-myStr += someAdjective
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+
+lastNameLength = lastName.length;
