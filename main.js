1e20da109b578f7d9e1064e881273d3e8b49956c
Jhonny <gonzodzen@mail.ru>
2022-11-28 21:39:40 +0300

    Use Recursion to Create a Countdown from freecodecamp

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function checkSign(num) {
-    return num>0?'positive':num<0?'negative':'zero'
-   }
-   
-   checkSign(10);
+// Only change code below this line
+function countdown(n){
+    if(n<1){
+      return []
+    }
+    else{
+      const countArr = countdown(n-1)
+      countArr.push(n)
+      return countArr.reverse().sort((a, b) => b-a)
+    }
+  }
+  // Only change code above this line
+  console.log(countdown(5))
