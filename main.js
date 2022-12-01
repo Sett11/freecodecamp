3cb52da35111a310599f74bef2041de1bdc7fc6d
Jhonny <gonzodzen@mail.ru>
2022-12-01 17:44:34 +0300

    Restrict Possible Usernames

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-let movieName = "2001: A Space Odyssey";
-let noNumRegex = /\D/g; // Change this line
-let result = movieName.match(noNumRegex).length;
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]([a-z]+\d*|\d{2,})$/i; // Change this line
+let result = userCheck.test(username);
