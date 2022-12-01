3c1bb429c7d975fb764fbb8deaecabcb6202d5c7
Jhonny <gonzodzen@mail.ru>
2022-12-01 12:36:04 +0300

    Use Destructuring Assignment to Assign Variables from Objects

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const HIGH_TEMPERATURES = {
 };
 
 // Only change code below this line
-
-const {today, tomorrow} = HIGH_TEMPERATURES;
+  
+const {today:highToday, tomorrow:highTomorrow} = HIGH_TEMPERATURES
 
 
 // Only change code above this line
