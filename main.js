fb7af375234eec3360c1cb4348772fb256bcc18a
Jhonny <gonzodzen@mail.ru>
2022-12-01 12:27:20 +0300

    Use the Spread Operator to Evaluate Arrays In-Place from freecodecamp

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const sum = (...args) => {
-  const arg = [...args];
-  return arg.reduce((a, b) => a + b, 0);
-}
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
