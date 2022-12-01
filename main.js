c6678cc63e2c456c81591373d560c64027ddf8a2
Jhonny <gonzodzen@mail.ru>
2022-12-01 16:59:07 +0300

    Match Characters that Occur Zero or More Times

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-let difficultSpelling = "Mississippi";
-let myRegex = /s+/ig; // Change this line
-let result = difficultSpelling.match(myRegex);
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
