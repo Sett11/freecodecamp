99e6aaa005e722cc35c8791cc64c98ab52831b62
Jhonny <gonzodzen@mail.ru>
2022-12-01 21:33:42 +0300

    Add Key-Value Pairs to JavaScript Objects

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-let myNestedArray = [
-  // Only change code below this line
-  ['unshift', false, 1,['deep',['deeper',['deepest']]], 2, 3, 'complex', 'nested'],
-  ['loop', 'shift', 6, 7, 1000, 'method'],
-  ['concat', false, true, 'spread', 'array'],
-  ['mutate', 1327.98, 'splice', 'slice', 'push'],
-  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
-  // Only change code above this line
-];
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+foods.bananas = 13
+foods.grapes = 35
+foods.strawberries = 27
+
+// Only change code below this line
+
+// Only change code above this line
+
+console.log(foods);
