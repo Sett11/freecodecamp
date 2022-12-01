55ef4a2d78abe355acf860befa188afb7ecfe851
Jhonny <gonzodzen@mail.ru>
2022-12-01 15:02:21 +0300

    Handle a Fulfilled Promise with then

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
-// Only change code below this line
-
-// Only change code above this line
-
-class Thermostat {
-  constructor(farengeit){
-    this._tempInCelcius = ((5/9) * (farengeit-32))
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
   }
-  get temperature(){
-    return this._tempInCelcius
-  }
-  set temperature(newTemp){
-    return this._tempInCelcius = newTemp
-  }
-}
-
-
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-; // 24.44 in Celsius
-
-console.log(thermos.setTemperature())
+})
+.then(result => console.log(result))
