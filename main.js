4274d86f0f6ee788ca7a2e6550e8a20726d312d2
Jhonny <gonzodzen@mail.ru>
2022-12-01 14:39:42 +0300

    Use getters and setters to Control Access to an Object

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 // Only change code below this line
 
 // Only change code above this line
-class Vegetable {
-  constructor(name){
-    this.name = name
+
+class Thermostat {
+  constructor(farengeit){
+    this._tempInCelcius = ((5/9) * (farengeit-32))
+  }
+  get temperature(){
+    return this._tempInCelcius
+  }
+  set temperature(newTemp){
+    return this._tempInCelcius = newTemp
   }
 }
 
 
-const carrot = new Vegetable('carrot');
-console.log(carrot.name); // Should display 'carrot'
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+; // 24.44 in Celsius
+
+console.log(thermos.setTemperature())
