b2caffa9ff2426b5511a8d75cd18b5010d5152ca
Jhonny <gonzodzen@mail.ru>
2022-12-01 22:07:08 +0300

    Generate an Array of All Object Keys with Object.keys()

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-const users = {
+let users = {
   Alan: {
+    age: 27,
     online: false
   },
   Jeff: {
+    age: 32,
     online: true
   },
   Sarah: {
+    age: 48,
     online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
   }
+};
+
+function getArrayOfUsers(obj) {
+ return Object.keys(obj)
 }
 
-function countOnline(usersObj) {
-  let counter = 0
-  for(let user in usersObj){
-    if(usersObj[user].online){
-      counter += 1
-    }
-  }
-  return counter
-}
-
-console.log(countOnline(users));
+console.log(getArrayOfUsers(users));
