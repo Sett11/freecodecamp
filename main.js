66d428312099a79ea652573ba2f5935ce70163ca
Jhonny <gonzodzen@mail.ru>
2022-12-01 12:25:25 +0300

    Use the Rest Parameter with Function Parameters from freecodecamp

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-// Only change code below this line
-const increment = (number, value = 1) => number + value;
-// Only change code above this line
+const sum = (...args) => {
+  const arg = [...args];
+  return arg.reduce((a, b) => a + b, 0);
+}
