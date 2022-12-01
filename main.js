02d6300e90e84f6a8c765cf4581fc096772818e6
Jhonny <gonzodzen@mail.ru>
2022-12-01 22:33:05 +0300

    Convert Celsius to Fahrenheit

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
-let user = {
-  name: 'Kenneth',
-  age: 28,
-  data: {
-    username: 'kennethCodesAllDay',
-    joinDate: 'March 26, 2016',
-    organization: 'freeCodeCamp',
-    friends: [
-      'Sam',
-      'Kira',
-      'Tomo'
-    ],
-    location: {
-      city: 'San Francisco',
-      state: 'CA',
-      country: 'USA'
-    }
-  }
-};
-
-function addFriend(userObj, friend) {
-  userObj.data.friends.push(friend)
- return userObj.data.friends
+function convertCtoF(celsius) {
+  let fahrenheit = celsius * (9/5) + 32
+  return fahrenheit;
 }
 
-console.log(addFriend(user, 'Pete'));
-console.log(user)
+convertCtoF(30);
