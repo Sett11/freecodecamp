98baa808913706a8443ba24cba3571a8a8043ab1
Jhonny <gonzodzen@mail.ru>
2022-12-01 19:42:43 +0300

    Understanding the Differences between the freeCodeCamp and Browser Console

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-let hello = "   Hello, World!  ";
-let wsRegex = /^(\s+)(.+[^\s])(\s+)$/; // Change this line
-let result = hello.replace(wsRegex, '$2'); // Change this line
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+console.log(a)
+let sumAB = a + b;
+console.log(sumAB);
