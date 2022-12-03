f53491eb3e1ffc896ecda1857d5c02acfd42e418
Jhonny <gonzodzen@mail.ru>
2022-12-03 15:53:22 +0300

    Sort an Array Alphabetically using the sort Method

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-const squareList = arr => {
-    return arr.filter(el=>Number.isInteger(el)&&el>0)
-    .map(el=>el**2)
+function alphabeticalOrder(arr) {
+    return arr.sort()
   }
   
-  const squaredIntegers = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
-  console.log(squaredIntegers);
+  console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]))
