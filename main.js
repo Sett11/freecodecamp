f556ab17d66dec14e86d9d62d9f3f8bedd01fb22
Jhonny <gonzodzen@mail.ru>
2022-12-03 16:42:16 +0300

    Use the some Method to Check that Any Elements in an Array Meet a Criteria

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function checkPositive(arr) {
     // Only change code below this line
-    return arr.every(val=>val>0)
+  return arr.some(val=>val>0)
   
     // Only change code above this line
   }
