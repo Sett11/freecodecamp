22fb0d41b0ca45192e70b7d3727938a0eed08d8d
Jhonny <gonzodzen@mail.ru>
2022-12-03 18:00:49 +0300

    Sum All Numbers in a Range

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
- const add = (x) => (y) => (z) => x+y+z
+function sumAll(arr) {
+    let i = arr[0]
+    let j = arr[1]
+    let newArr = []
+    if(i>j){
+        while(j<=i){
+            newArr.push(j)
+            j++
+        }
+    }
+    else{
+        while(i<=j){
+            newArr.push(i)
+            i++
+        }
+    }
+    return newArr.reduce((acc, cur)=>acc+cur)
+  }
   
-  console.log(add(10)(20)(30))
+  console.log(sumAll([5,10]))
