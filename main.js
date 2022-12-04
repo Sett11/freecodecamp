d777b271dee3e646a0e82bbde999c5cbd5acc0de
Jhonny <gonzodzen@mail.ru>
2022-12-04 17:02:26 +0300

    Sorted Union

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
-function myReplace(str, before, after) {
-    let sen = str.split(' ')
-    for(let i = 0; i< sen.length; i++){
-        if(sen[i] == before){
-            let wor = sen[i]
-            if(/[A-Z]/.test(wor[0])){
-                let af = after.charAt(0).toUpperCase() + after.slice(1)
-                sen.splice(i, 1, af)
-            }
-            else if(/[a-z]/.test(wor[0])){
-                let af = after.charAt(0).toLowerCase() + after.slice(1)
-                sen.splice(i, 1, af)
-            }
-            else{
-                sen.splice(i, 1, after)
-            }
-        }
-    }
-    return sen.join(' ')
+function uniteUnique(...arr) {
+    let newArr = [...arr].flat()
+    return [...new Set(newArr)]
   }
   
-  console.log(myReplace("I think we should look up there", "up", "Down"))
-  
+  console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
