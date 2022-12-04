173d3b9cd6bca6812838abcce2ecf875cd328cab
Jhonny <gonzodzen@mail.ru>
2022-12-04 15:24:05 +0300

    Wherefore art thou

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-const f = x => y => z => x+y+z
-  console.log(f(10)(20)(30))
+function whatIsInAName(collection, source) {
+    let arr = []
+    arr = collection.filter(obj => {
+        let soKeys = Object.keys(source)
+        for(let i = 0; i < soKeys.length; i++){
+            if(obj.hasOwnProperty(soKeys[i])){
+                if(obj[soKeys[i]] != source[soKeys[i]]){
+                    return false
+                }
+            }
+            else{
+                return false
+            }
+        }
+        return true
+    })
+    return arr
+}
+
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
