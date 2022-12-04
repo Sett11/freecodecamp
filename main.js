b744e55ce51fad6212d33b6d052d806298218d37
Jhonny <gonzodzen@mail.ru>
2022-12-04 14:06:53 +0300

    Seek and Destroy

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-const f = (x) => (y) => (z) => (v) => (x+y+z) * v
-console.log(f(10)(20)(30)(2))
+function destroyer(arr) {
+    let newArr = []
+    for(let i = 0; i < arguments.length; i++){
+        newArr[i] = arguments[i]
+    }
+     let nextMyArr = newArr.shift()
+    return nextMyArr.filter(el => newArr.indexOf(el) === -1)
+  }
+  
+  console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5))
