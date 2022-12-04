22f1cd095536143e82710025f31236a4ad28ab8f
Jhonny <gonzodzen@mail.ru>
2022-12-04 13:19:37 +0300

    My craft carring!

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-function diffArray(arr1, arr2) {
-    let newArr = arr1.concat(arr2)
-    return newArr.filter((el) => arr1.indexOf(el) === -1 || arr2.indexOf(el) === -1)
-  }
-  
-  console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]))
+const f = (x) => (y) => (z) => (v) => (x+y+z) * v
+console.log(f(10)(20)(30)(2))
