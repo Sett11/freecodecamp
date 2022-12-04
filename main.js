35030243458a6eccef1ed7756bdc196bd3285590
Jhonny <gonzodzen@mail.ru>
2022-12-04 17:56:39 +0300

    Sum All Odd Fibonacci Numbers

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function uniteUnique(...arr) {
-    let newArr = [...arr].flat()
-    return [...new Set(newArr)]
+function sumFibs(num) {
+    if(num === 1){
+        return num
+    }
+    let arr = [1, 1]
+    for(let i = 2; i < num; i++){
+        arr[i] = arr[i-1] + arr[i-2]
+    }
+    return arr.slice(0, num)
+    .filter(el => el<=num && el%2!==0)
+    .reduce((acc, cur) => acc + cur)
   }
   
-  console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
+  console.log(sumFibs(4))
