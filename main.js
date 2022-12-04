1e41fa3cc2eef78e52a5653ace5b02826bda2fb2
Jhonny <gonzodzen@mail.ru>
2022-12-04 23:55:08 +0300

    Drop it

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-function binaryAgent(str) {
-  let arr = str.split(' ')
+function dropElements(arr, func) {
   let newArr = []
 
   for(let i = 0; i < arr.length; i++){
-    newArr.push(String.fromCharCode(parseInt(arr[i], 2)))
+    if(func(arr[i])){
+      newArr = arr.slice(i)
+      break
+    }
   }
-  return newArr.join('')
+  return newArr
 }
 
-console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2}))
