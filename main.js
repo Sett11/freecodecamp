6fc0e0b8b7701ccd0b5ba2dc2d9f2f4d04a3ee09
Jhonny <gonzodzen@mail.ru>
2022-12-04 21:22:33 +0300

    Roman Numeral Converter

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-const Person = function (firstAndLast) {
-  let fullName = firstAndLast;
-  let s = fullName.split(" ");
-  this.getFirstName = () => {
-    return s[0];
-  };
-  this.getLastName = () => {
-    return s[1];
-  };
-  this.getFullName = () => {
-    return this.getFirstName() + " " + this.getLastName();
-  };
-  this.setFirstName = (first) => {
-    s[0] = first;
-  };
-  this.setLastName = (last) => {
-    s[1] = last;
-  };
-  this.setFullName = (firstAndLast) => {
-    let fullName = firstAndLast;
-    s = fullName.split(" ");
-  };
-};
+function convertToRoman(number) {
+    let roman = {
+      "M": 1000,
+      "CM": 900,
+      "D": 500,
+      "CD": 400,
+      "C": 100,
+      "XC": 90,
+      "L": 50,
+      "XL": 40,
+      "X": 10,
+      "IX": 9,
+      "V": 5,
+      "IV": 4,
+      "I": 1
+    }
+    let result = ''
+    for(let i of Object.keys(roman)){
+        let repeat = Math.floor(number/roman[i])
+        number -= repeat * roman[i]
+        result += i.repeat(repeat)
+    }
+    return result
+}
 
-const bob = new Person("Bob Ross");
-const rob = new Person("Rob Loh");
-
-console.log(bob.getFullName());
-console.log(rob.getFullName());
-
-bob.setFullName("Vasia Pupkin");
-console.log(bob.getFullName());
+  
+  console.log(convertToRoman(5))
